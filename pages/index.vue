d0672ba7965188a9625054d9ecaf865a7ff8503e
NATTAWUT_IN <150765583+nattawut29427@users.noreply.github.com>
2024-01-30 23:23:04 +0700

    เพิ่ม banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
 <template>
-  <Header/>
+  <Header />
+  <div class="h-screen bg-gray-200 w-full grid place-items-center">
+    <div
+      class="w-[910px] h-[312px] bg-bluehover mb-[200px] flex justify-center text-center items-center"
+    >
+      <h1 class="text-white text-[100px]">พื้นที่โฆษณา</h1>
+    </div>
+  </div>
 </template>
